e3dd4f7fb0a2d86d78fbb80ddaa7de70b3b5c478
shammmyyy <129077959+shammmyyy@users.noreply.github.com>
2024-05-28 23:23:27 +0800

    Update Module3.sol

--- Module3.sol
+++ Module3.sol
@@ -1,37 +1,45 @@
-//SPDX-License-Identifier: MIT
-  pragma solidity ^0.8.15;
-        
-        contract Etherium{
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-            address public Main;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-            constructor(address Owner){
-                Main = Owner;
-            }
-                    mapping (address Address => uint Etherium)public Balance;
+contract Etherium is ERC20, Ownable {
+    uint256 private _maxOwnerMint;
+    mapping(address => uint256) private _ownerMintedAmounts;
 
-                function Mint (address Sender, uint token)public{
-                        if(Sender == Main){
-                            Balance[Sender] += token;
-                        }else{
-                            revert("Only owner can mint the token");
-                        }
-                }
+    constructor(address owner) ERC20("Etherium", "ETH") Ownable(owner) {
+        // Initialize the maximum owner mint to 0
+        _maxOwnerMint = 0;
+    }
 
-                function Burn (address Sender, uint token)public{
-                       if(Balance[Sender] <= token){
-                            revert("don't have enough balances");
-                       }else{
-                            Balance[Sender] -= token;
-                       } 
-                }
+    modifier onlyUnderMaxOwnerMint(uint256 amount) {
+        require(_ownerMintedAmounts[msg.sender] + amount <= _maxOwnerMint, "Exceeds max owner mint");
+        _;
+    }
 
-                function Transfer (address OwnerAccount, address OtherAccount, uint token)public{
-                        if(Balance[OwnerAccount] <= token){
-                            revert("don't have enough balance");
-                       }else{
-                            Balance[OwnerAccount] -= token;
-                            Balance[OtherAccount] += token;
-                       }
-                }
-         }
+    function mint(address to, uint256 amount) public onlyOwner onlyUnderMaxOwnerMint(amount) {
+        _mint(to, amount);
+        _ownerMintedAmounts[msg.sender] += amount;
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        return super.transfer(to, amount);
+    }
+
+    function setMaxOwnerMint(uint256 maxOwnerMint) public onlyOwner {
+        _maxOwnerMint = maxOwnerMint;
+    }
+
+    function getMaxOwnerMint() public view returns (uint256) {
+        return _maxOwnerMint;
+    }
+
+    function getOwnerMintedAmount(address owner) public view returns (uint256) {
+        return _ownerMintedAmounts[owner];
+    }
+}
